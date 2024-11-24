733e0b219b25319d90a22480ead06cb90b7d39d7
Avanthika-Yervala <avanthikareddy169@gmail.com>
2024-11-25 01:43:40 +0530

    Update Fourthcode.java
    add syso statment

--- Fourthcode.java
+++ Fourthcode.java
@@ -1,5 +1,7 @@
 public class Fourthcode {
     public static void main(String[] args) {
         System.out.println("avanthika,how are you !!!");
+         System.out.println("Iam good 😍😍");
+        
     }
 }
